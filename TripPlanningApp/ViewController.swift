a38ab7a719827ec83766ace49f883e6206850862
Dinesh <34174856+dinesh2910@users.noreply.github.com>
2020-06-01 23:04:37 -0500

    Initial Commit

---
+++ TripPlanningApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TripPlanningApp
+//
+//  Created by Dinesh Danda on 6/1/20.
+//  Copyright © 2020 Dinesh Danda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
