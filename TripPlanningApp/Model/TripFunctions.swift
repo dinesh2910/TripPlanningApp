16326f08e4c59beadf454601b812e39358a7ba6d
Dinesh <34174856+dinesh2910@users.noreply.github.com>
2020-06-02 15:36:04 -0500

    Model Class Initial SetUp

---
+++ TripPlanningApp/Model/TripFunctions.swift
@@ -0,0 +1,30 @@
+//
+//  TripFunctions.swift
+//  TripPlanningApp
+//
+//  Created by Dinesh Danda on 6/2/20.
+//  Copyright © 2020 Dinesh Danda. All rights reserved.
+//
+
+import Foundation
+
+
+class TripFunctions {
+    
+    static func createTrip(tripModel: TripModel) {
+        
+    }
+    
+    static func readTrips() {
+        
+    }
+    
+    static func updateTrip(tripModel: TripModel) {
+        
+    }
+    
+    static func deleteTrip(tripModel: TripModel) {
+        
+    }
+    
+}
---
+++ TripPlanningApp/Model/TripModel.swift
@@ -0,0 +1,21 @@
+//
+//  TripModel.swift
+//  TripPlanningApp
+//
+//  Created by Dinesh Danda on 6/2/20.
+//  Copyright © 2020 Dinesh Danda. All rights reserved.
+//
+
+import Foundation
+
+
+class TripModel {
+    
+    var id: String
+    var tripTitle: String
+    
+    init(tripTitle: String) {
+        id = UUID().uuidString
+        self.tripTitle = tripTitle
+    }
+}
