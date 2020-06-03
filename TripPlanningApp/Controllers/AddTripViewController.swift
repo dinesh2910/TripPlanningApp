2f592b5bb5579efa5d891c70e28f564fe73a570d
Dinesh <34174856+dinesh2910@users.noreply.github.com>
2020-06-03 01:17:34 -0500

    PopUp's Initial Commit.

---
+++ TripPlanningApp/Controllers/AddTripViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AddTripViewController.swift
+//  TripPlanningApp
+//
+//  Created by Dinesh Danda on 6/3/20.
+//  Copyright © 2020 Dinesh Danda. All rights reserved.
+//
+
+import UIKit
+
+class AddTripViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
